d402ef0d13b7d24d843d69147c6235084cb5c010
coder <coderww@163.com>
2022-04-25 15:22:26 +0800

    集成yolox，并且实现label展示视频帧

--- PipeVideo.py
+++ PipeVideo.py
@@ -1,9 +1,15 @@
+import os
 import sys
+import time
 
+import cv2
+import torch
 from PyQt5.QtCore import QUrl
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from PyQt5.QtWidgets import QMainWindow, QFileDialog, QApplication
+from PyQt5 import QtGui
 
+from models.yolox.yoloxs import Predictor, imageflow_demo, CLASSES
 from test import Ui_MainWindow
 from myCreateProject import myCreateProjects
 from Project import Project
@@ -14,14 +20,15 @@ class myMainWindow(Ui_MainWindow, QMainWindow):
         super(Ui_MainWindow, self).__init__()
         self.setupUi(self)
         self.sld_video_pressed = False  #判断当前进度条识别否被鼠标点击
-        self.player = QMediaPlayer()
-        self.player.setVideoOutput(self.widget)  # 父类继承的
+        # self.player = QMediaPlayer()
+        # self.player.setVideoOutput(self.widget)  # 父类继承的
         self.action.triggered.connect(self.createProject)
         self.action_2.triggered.connect(self.selectProject)
         self.action_3.triggered.connect(self.saveProject)
+        self.action_5.triggered.connect(self.inferenceVideo)
         self.pushButton_2.clicked.connect(self.playVideo)       # play
         self.pushButton.clicked.connect(self.pauseVideo)       # pause
-        self.player.positionChanged.connect(self.changeSlide)  # change Slide
+        # self.player.positionChanged.connect(self.changeSlide)  # change Slide
         self.horizontalSlider_2.setTracking(False)
         self.horizontalSlider_2.sliderReleased.connect(self.releaseSlider)
         self.horizontalSlider_2.sliderPressed.connect(self.pressSlider)
@@ -35,7 +42,7 @@ class myMainWindow(Ui_MainWindow, QMainWindow):
     def openVideoFile(self):
         # self.player.setMedia(QMediaContent(QFileDialog.getOpenFileUrl()[0]))  # 选取视频文件
         # 用于测试功能，直接播放
-        self.player.setMedia(QMediaContent(QUrl.fromLocalFile(r'C:\Users\coder\PycharmProjects\pyqt\resources\test1.mp4'))) #直接播放
+        self.player.setMedia(QMediaContent(QUrl.fromLocalFile('./resources/remove1.avi'))) #直接播放
         self.player.play()  # 播放视频
         print(self.player.availableMetaData())
 
@@ -45,11 +52,11 @@ class myMainWindow(Ui_MainWindow, QMainWindow):
     def pauseVideo(self):
         self.player.pause()
 
-    def changeSlide(self, position):
-        if not self.sld_video_pressed:  # 进度条被鼠标点击时不更新
-            self.vidoeLength = self.player.duration()+0.1
-            self.horizontalSlider_2.setValue(round((position/self.vidoeLength)*100))
-            self.label_17.setText("%.2f%%" % ((position/self.vidoeLength)*100))
+    # def changeSlide(self, position):
+    #     if not self.sld_video_pressed:  # 进度条被鼠标点击时不更新
+    #         self.vidoeLength = self.player.duration()+0.1
+    #         self.horizontalSlider_2.setValue(round((position/self.vidoeLength)*100))
+    #         self.label_17.setText("%.2f%%" % ((position/self.vidoeLength)*100))
 
     def releaseSlider(self):
         self.sld_video_pressed = False
@@ -73,10 +80,8 @@ class myMainWindow(Ui_MainWindow, QMainWindow):
         else:
             self.horizontalSlider_2.setValue(0)
 
-
     def createProject(self):
         self.crtPrj.show()
-
 
     #2022-3-8 选择项目打开
     def selectProject(self):
@@ -92,6 +97,52 @@ class myMainWindow(Ui_MainWindow, QMainWindow):
         projectName = Project()
         projectName.Serialization()
 
+    def inferenceVideo(self):
+        file_name = "./models/yolox/myyolox/"
+        os.makedirs(file_name, exist_ok=True)
+        vis_folder = os.path.join(file_name, "vis_res")
+        os.makedirs(vis_folder, exist_ok=True)
+
+        model = torch.load("./models/yolox/yolox_detector.pth")
+        print("模型加载完成")
+        model.cuda()
+        # model.half()
+        model.eval()
+        predictor = Predictor(model, CLASSES)
+        current_time = time.localtime()
+
+        video_path = "./resources/remove1.avi"
+        cap = cv2.VideoCapture(video_path)
+        width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
+        height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        save_folder = os.path.join(
+            vis_folder, time.strftime("%Y_%m_%d_%H_%M_%S", current_time)
+        )
+        os.makedirs(save_folder, exist_ok=True)
+        # save_path需要看软件需要
+        save_path = os.path.join(save_folder, video_path.split("/")[-1])
+        vid_writer = cv2.VideoWriter(
+            save_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (int(width), int(height))
+        )
+        print("开始预测")
+        while True:
+            ret_val, frame = cap.read()
+            if ret_val:
+                outputs, img_info = predictor.inference(frame)
+                result_frame = predictor.visual(outputs[0], img_info, predictor.confthre)
+                _image = QtGui.QImage(result_frame[:], result_frame.shape[1], result_frame.shape[0], result_frame.shape[1] * 3,
+                                      QtGui.QImage.Format_RGB888)  # pyqt5转换成自己能放的图片格式
+                jpg_out = QtGui.QPixmap(_image).scaled(self.label.width(), self.label.height())  # 设置图片大小
+                self.label.setPixmap(jpg_out)  # 设置图片显示
+                vid_writer.write(result_frame)
+                # 这些地方应该能改一改作为pyqt暂停的信号槽
+                # ch = cv2.waitKey(1)
+                # if ch == 27 or ch == ord("q") or ch == ord("Q"):
+                #     break
+            else:
+                break
+        print("检测完成")
 
     # def volumeChange(self, position):
     #     volume = round(position/self.sld_audio.maximum()*100)
---
+++ models/yolox/yoloxs.py
@@ -0,0 +1,364 @@
+# 用于集成到软件上
+
+import cv2
+import numpy as np
+import torch
+import os
+import time
+
+import torchvision
+from PyQt5 import QtGui
+
+CLASSES = ("FS", "PL")
+
+def myPreproc(img, input_size, swap=(2, 0, 1)):
+    if len(img.shape) == 3:
+        padded_img = np.ones((input_size[0], input_size[1], 3), dtype=np.uint8) * 114
+    else:
+        padded_img = np.ones(input_size, dtype=np.uint8) * 114
+
+    r = min(input_size[0] / img.shape[0], input_size[1] / img.shape[1])
+    resized_img = cv2.resize(
+        img,
+        (int(img.shape[1] * r), int(img.shape[0] * r)),
+        interpolation=cv2.INTER_LINEAR,
+    ).astype(np.uint8)
+    padded_img[: int(img.shape[0] * r), : int(img.shape[1] * r)] = resized_img
+
+    padded_img = padded_img.transpose(swap)
+    padded_img = np.ascontiguousarray(padded_img, dtype=np.float32)
+    return padded_img, r
+
+class ValTransform:
+    """
+    Defines the transformations that should be applied to test PIL image
+    for input into the network
+
+    dimension -> tensorize -> color adj
+
+    Arguments:
+        resize (int): input dimension to SSD
+        rgb_means ((int,int,int)): average RGB of the dataset
+            (104,117,123)
+        swap ((int,int,int)): final order of channels
+
+    Returns:
+        transform (transform) : callable transform to be applied to test/val
+        data
+    """
+
+    def __init__(self, swap=(2, 0, 1), legacy=False):
+        self.swap = swap
+        self.legacy = legacy
+
+    # assume input is cv2 img for now
+    def __call__(self, img, res, input_size):
+        img, _ = myPreproc(img, input_size, self.swap)
+        if self.legacy:
+            img = img[::-1, :, :].copy()
+            img /= 255.0
+            img -= np.array([0.485, 0.456, 0.406]).reshape(3, 1, 1)
+            img /= np.array([0.229, 0.224, 0.225]).reshape(3, 1, 1)
+        return img, np.zeros((1, 5))
+
+def postprocess(prediction, num_classes, conf_thre=0.7, nms_thre=0.45, class_agnostic=False):
+    box_corner = prediction.new(prediction.shape)
+    box_corner[:, :, 0] = prediction[:, :, 0] - prediction[:, :, 2] / 2
+    box_corner[:, :, 1] = prediction[:, :, 1] - prediction[:, :, 3] / 2
+    box_corner[:, :, 2] = prediction[:, :, 0] + prediction[:, :, 2] / 2
+    box_corner[:, :, 3] = prediction[:, :, 1] + prediction[:, :, 3] / 2
+    prediction[:, :, :4] = box_corner[:, :, :4]
+
+    output = [None for _ in range(len(prediction))]
+    for i, image_pred in enumerate(prediction):
+
+        # If none are remaining => process next image
+        if not image_pred.size(0):
+            continue
+        # Get score and class with highest confidence
+        class_conf, class_pred = torch.max(image_pred[:, 5: 5 + num_classes], 1, keepdim=True)
+
+        conf_mask = (image_pred[:, 4] * class_conf.squeeze() >= conf_thre).squeeze()
+        # Detections ordered as (x1, y1, x2, y2, obj_conf, class_conf, class_pred)
+        detections = torch.cat((image_pred[:, :5], class_conf, class_pred.float()), 1)
+        detections = detections[conf_mask]
+        if not detections.size(0):
+            continue
+
+        if class_agnostic:
+            nms_out_index = torchvision.ops.nms(
+                detections[:, :4],
+                detections[:, 4] * detections[:, 5],
+                nms_thre,
+            )
+        else:
+            nms_out_index = torchvision.ops.batched_nms(
+                detections[:, :4],
+                detections[:, 4] * detections[:, 5],
+                detections[:, 6],
+                nms_thre,
+            )
+
+        detections = detections[nms_out_index]
+        if output[i] is None:
+            output[i] = detections
+        else:
+            output[i] = torch.cat((output[i], detections))
+
+    return output
+
+_COLORS = np.array(
+    [
+        0.000, 0.447, 0.741,
+        0.850, 0.325, 0.098,
+        0.929, 0.694, 0.125,
+        0.494, 0.184, 0.556,
+        0.466, 0.674, 0.188,
+        0.301, 0.745, 0.933,
+        0.635, 0.078, 0.184,
+        0.300, 0.300, 0.300,
+        0.600, 0.600, 0.600,
+        1.000, 0.000, 0.000,
+        1.000, 0.500, 0.000,
+        0.749, 0.749, 0.000,
+        0.000, 1.000, 0.000,
+        0.000, 0.000, 1.000,
+        0.667, 0.000, 1.000,
+        0.333, 0.333, 0.000,
+        0.333, 0.667, 0.000,
+        0.333, 1.000, 0.000,
+        0.667, 0.333, 0.000,
+        0.667, 0.667, 0.000,
+        0.667, 1.000, 0.000,
+        1.000, 0.333, 0.000,
+        1.000, 0.667, 0.000,
+        1.000, 1.000, 0.000,
+        0.000, 0.333, 0.500,
+        0.000, 0.667, 0.500,
+        0.000, 1.000, 0.500,
+        0.333, 0.000, 0.500,
+        0.333, 0.333, 0.500,
+        0.333, 0.667, 0.500,
+        0.333, 1.000, 0.500,
+        0.667, 0.000, 0.500,
+        0.667, 0.333, 0.500,
+        0.667, 0.667, 0.500,
+        0.667, 1.000, 0.500,
+        1.000, 0.000, 0.500,
+        1.000, 0.333, 0.500,
+        1.000, 0.667, 0.500,
+        1.000, 1.000, 0.500,
+        0.000, 0.333, 1.000,
+        0.000, 0.667, 1.000,
+        0.000, 1.000, 1.000,
+        0.333, 0.000, 1.000,
+        0.333, 0.333, 1.000,
+        0.333, 0.667, 1.000,
+        0.333, 1.000, 1.000,
+        0.667, 0.000, 1.000,
+        0.667, 0.333, 1.000,
+        0.667, 0.667, 1.000,
+        0.667, 1.000, 1.000,
+        1.000, 0.000, 1.000,
+        1.000, 0.333, 1.000,
+        1.000, 0.667, 1.000,
+        0.333, 0.000, 0.000,
+        0.500, 0.000, 0.000,
+        0.667, 0.000, 0.000,
+        0.833, 0.000, 0.000,
+        1.000, 0.000, 0.000,
+        0.000, 0.167, 0.000,
+        0.000, 0.333, 0.000,
+        0.000, 0.500, 0.000,
+        0.000, 0.667, 0.000,
+        0.000, 0.833, 0.000,
+        0.000, 1.000, 0.000,
+        0.000, 0.000, 0.167,
+        0.000, 0.000, 0.333,
+        0.000, 0.000, 0.500,
+        0.000, 0.000, 0.667,
+        0.000, 0.000, 0.833,
+        0.000, 0.000, 1.000,
+        0.000, 0.000, 0.000,
+        0.143, 0.143, 0.143,
+        0.286, 0.286, 0.286,
+        0.429, 0.429, 0.429,
+        0.571, 0.571, 0.571,
+        0.714, 0.714, 0.714,
+        0.857, 0.857, 0.857,
+        0.000, 0.447, 0.741,
+        0.314, 0.717, 0.741,
+        0.50, 0.5, 0
+    ]
+).astype(np.float32).reshape(-1, 3)
+
+def vis(img, boxes, scores, cls_ids, conf=0.5, class_names=None):
+
+    for i in range(len(boxes)):
+        box = boxes[i]
+        cls_id = int(cls_ids[i])
+        score = scores[i]
+        if score < conf:
+            continue
+        x0 = int(box[0])
+        y0 = int(box[1])
+        x1 = int(box[2])
+        y1 = int(box[3])
+
+        color = (_COLORS[cls_id] * 255).astype(np.uint8).tolist()
+        text = '{}:{:.1f}%'.format(class_names[cls_id], score * 100)
+        txt_color = (0, 0, 0) if np.mean(_COLORS[cls_id]) > 0.5 else (255, 255, 255)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+
+        txt_size = cv2.getTextSize(text, font, 0.4, 1)[0]
+        cv2.rectangle(img, (x0, y0), (x1, y1), color, 2)
+
+        txt_bk_color = (_COLORS[cls_id] * 255 * 0.7).astype(np.uint8).tolist()
+        cv2.rectangle(
+            img,
+            (x0, y0 + 1),
+            (x0 + txt_size[0] + 1, y0 + int(1.5*txt_size[1])),
+            txt_bk_color,
+            -1
+        )
+        cv2.putText(img, text, (x0, y0 + txt_size[1]), font, 0.4, txt_color, thickness=1)
+
+    return img
+
+
+class Predictor(object):
+    def __init__(
+        self,
+        model,
+        cls_names=CLASSES,
+        trt_file=None,
+        decoder=None,
+        device="gpu",
+        fp16=False,
+        legacy=False,
+    ):
+        self.model = model
+        self.cls_names = cls_names
+        self.decoder = decoder
+        self.num_classes = 2
+        self.confthre = 0.01
+        self.nmsthre = 0.65
+        self.test_size = (640, 640)
+        self.device = device
+        self.fp16 = fp16
+        self.preproc = ValTransform(legacy=legacy)
+        if trt_file is not None:
+            from torch2trt import TRTModule
+
+            model_trt = TRTModule()
+            model_trt.load_state_dict(torch.load(trt_file))
+
+            x = torch.ones(1, 3, self.test_size[0], self.test_size[1]).cuda()
+            self.model(x)
+            self.model = model_trt
+
+    def inference(self, img):
+        img_info = {"id": 0}
+        if isinstance(img, str):
+            img_info["file_name"] = os.path.basename(img)
+            img = cv2.imread(img)
+        else:
+            img_info["file_name"] = None
+
+        height, width = img.shape[:2]
+        img_info["height"] = height
+        img_info["width"] = width
+        img_info["raw_img"] = img
+
+        ratio = min(self.test_size[0] / img.shape[0], self.test_size[1] / img.shape[1])
+        img_info["ratio"] = ratio
+
+        img, _ = self.preproc(img, None, self.test_size)
+        img = torch.from_numpy(img).unsqueeze(0)
+        img = img.float()
+        if self.device == "gpu":
+            img = img.cuda()
+            if self.fp16:
+                img = img.half()  # to FP16
+
+        with torch.no_grad():
+            t0 = time.time()
+            outputs = self.model(img)
+            if self.decoder is not None:
+                outputs = self.decoder(outputs, dtype=outputs.type())
+            outputs = postprocess(
+                outputs, self.num_classes, self.confthre,
+                self.nmsthre, class_agnostic=True
+            )
+        return outputs, img_info
+
+    def visual(self, output, img_info, cls_conf=0.35):
+        ratio = img_info["ratio"]
+        img = img_info["raw_img"]
+        if output is None:
+            return img
+        output = output.cpu()
+
+        bboxes = output[:, 0:4]
+
+        # preprocessing: resize
+        bboxes /= ratio
+
+        cls = output[:, 6]
+        scores = output[:, 4] * output[:, 5]
+
+        vis_res = vis(img, bboxes, scores, cls, cls_conf, self.cls_names)
+        return vis_res
+
+
+def imageflow_demo(predictor, vis_folder, current_time):
+    video_path = "./resources/remove1.avi"
+    cap = cv2.VideoCapture(video_path)
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    save_folder = os.path.join(
+        vis_folder, time.strftime("%Y_%m_%d_%H_%M_%S", current_time)
+    )
+    os.makedirs(save_folder, exist_ok=True)
+    # save_path需要看软件需要
+    save_path = os.path.join(save_folder, video_path.split("/")[-1])
+    vid_writer = cv2.VideoWriter(
+        save_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (int(width), int(height))
+    )
+    while True:
+        ret_val, frame = cap.read()
+        if ret_val:
+            outputs, img_info = predictor.inference(frame)
+            result_frame = predictor.visual(outputs[0], img_info, predictor.confthre)
+            # _image = QtGui.QImage(result_frame[:], result_frame.shape[1], result_frame.shape[0], result_frame.shape[1] * 3,
+            #                       QtGui.QImage.Format_RGB888)  # pyqt5转换成自己能放的图片格式
+            # jpg_out = QtGui.QPixmap(_image).scaled(thesw.Label.width(), thesw.Label.height())  # 设置图片大小
+            # thesw.label.setPixmap(jpg_out)  # 设置图片显示
+            vid_writer.write(result_frame)
+            # 这些地方应该能改一改作为pyqt暂停的信号槽
+            # ch = cv2.waitKey(1)
+            # if ch == 27 or ch == ord("q") or ch == ord("Q"):
+            #     break
+        else:
+            break
+
+# def main():
+#     # 保存的位置，如果不存在就创建
+#     file_name = "./myyolox/"
+#     os.makedirs(file_name, exist_ok=True)
+#     vis_folder = os.path.join(file_name, "vis_res")
+#     os.makedirs(vis_folder, exist_ok=True)
+#
+#     model = torch.load("C:/Users/coder/PycharmProjects/dlqt_728/models11/yolox/yolox_detector.pth")
+#     print("模型加载完成")
+#     model.cuda()
+#     # model.half()
+#     model.eval()
+#     predictor = Predictor(model, CLASSES)
+#     current_time = time.localtime()
+#     imageflow_demo(predictor, vis_folder, current_time)
+#     print("检测完成")
+#
+# if __name__ == "__main__":
+#     main()
